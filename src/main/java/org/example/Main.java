8b5deb80635d604d29fa9af6bab1a631ded9712c
chenhaohao01 <1716282060@qq.com>
2024-01-30 18:05:32 +0800

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hahhaha上传成功了");
         System.out.println("Hello world!");
     }
 }
